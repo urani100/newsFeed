a065a67efc17e3ad264eb4cb7bce1b685390bbf4
M L <mlecontenyc@gmail.com>
2019-02-27 16:24:20 -0500

    working on notes

--- server.js
+++ server.js
@@ -101,7 +101,21 @@ app.get("/savedartcl", function(req, res) {
 	});
 });
 
-
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/savedartcl/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 
 
