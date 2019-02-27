9320fb7ea8627c67f131540763dad0d3474375d0
M L <mlecontenyc@gmail.com>
2019-02-27 17:21:58 -0500

    working on notes

--- models/Note.js
+++ models/Note.js
@@ -6,10 +6,18 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new NoteSchema object
 // This is similar to a Sequelize model
 var NoteSchema = new Schema({
-  // `title` is of type String
-  title: String,
-  // `body` is of type String
-  body: String
+  title: {
+    type: String,
+    required: false
+  },
+  body: {
+    type: String,
+    required: false
+  },
+  // // `title` is of type String
+  // title: String,
+  // // `body` is of type String
+  // body: String
 });
 
 // This creates our model from the above schema, using mongoose's model method
--- public/app.js
+++ public/app.js
@@ -91,16 +91,15 @@ $(function(){
 
 });    
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  //show modal
-  // $('#results-modal').modal('show');
-  
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
 
+//Notes
+// Whenever someone clicks  add notes
+$(document).on("click", "#addNote", function() {
+  // // Empty the notes from the note section
+  $("#notes").empty();
+  // // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+  
   // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
@@ -126,4 +125,33 @@ $(document).on("click", "p", function() {
         $("#bodyinput").val(data.note.body);
       }
     });
+});
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    dataType: 'json',
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val(),
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
 });
